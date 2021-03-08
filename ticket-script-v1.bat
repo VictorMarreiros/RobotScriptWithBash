f3f2af2fcb10c45e5b9b47d8bc16f9596a93bc2c
Victor Marreiros <v.marreiros1@gmail.com>
2021-03-08 16:07:38 -0300

    modificado caminho do projeto para './script/src'

--- ticket-script-v1.bat
+++ ticket-script-v1.bat
@@ -26,7 +26,7 @@ if %opcao% GEQ 3 goto opcao3
 :opcao1
 title Abrir ticket - Revenda
 set /P id_cli=Informe o ID Cliente: 
-python -m robot --variable id_cliente:%id_cli% C:/Projects/ScriptRobot/ticket-revenda.robot
+python -m robot --variable id_cliente:%id_cli% C:/script/src/ticket-revenda.robot
 pause
 goto menu
 
